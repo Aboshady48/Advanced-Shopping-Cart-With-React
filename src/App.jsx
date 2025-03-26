1fbe9b77865a3167384eafd6b73a1081752f80d3
Aboshady48 <aboshady48585@gmail.com>
2025-03-27 00:30:28 +0200

    Create the Routes oF The Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import Navbar from './Components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Store from './Pages/Store'
+import About from './Pages/About'
 const App = () => {
   return (
-    <div>
-      
+    <div className='container'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/store' element={<Store/>} />
+        <Route path='/about' element={<About/>} />
+      </Routes>      
     </div>
   )
 }
---
+++ src/Pages/Store.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Store = () => {
+  return (
+    <div>
+        <h1>Our Store</h1>
+        <p>Welcome to our Store Page</p>
+    </div>
+  )
+}
+
+export default Store
